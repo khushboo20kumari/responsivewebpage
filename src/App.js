2f296e6bd3d1a627c569bbc1ddff5486d3ae8f32
Khushboo Kumari <khushboo20@navgurukul.org>
2023-12-08 12:55:55 +0530

    SideBar and Navbar are completed ui part only

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SideBar from "./Layout/SideBar"
+function App() {
+  return (
+    <div className="App">
+      <SideBar/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layout/NavBar.js
@@ -0,0 +1,53 @@
+import MuiAppBar from '@mui/material/AppBar';
+import { styled} from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+const drawerWidth = 240;
+function NavBar({ open, handleDrawerOpen }) {
+
+
+    const AppBar = styled(MuiAppBar, {
+        shouldForwardProp: (prop) => prop !== 'open',
+    })(({ theme, open }) => ({
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(open && {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        }),
+    }));
+
+    return (
+        <>
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Mini variant drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+export default NavBar;
